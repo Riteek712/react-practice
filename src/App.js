2ba991708aec02cb4e78b0a4bdab09ec31bcfd89
Riteek <riteek7.12@gmail.com>
2023-02-04 14:33:42 +0530

    react first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './Header';
+import Contents from './Contents';
+import Footer from './Footer';
 function App() {
+  const handleNameChange = () => {
+    const name  = ["rik", "dik", "pik"];
+    const int = Math.floor(Math.random()*3);
+
+    return name[int];
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Contents />
+      <Footer />
     </div>
   );
 }
---
+++ src/Contents.js
@@ -0,0 +1,13 @@
+
+
+const Contents = () => {
+  return (
+    <main>
+        <h1>
+            HI riteek, you better learn react fast.
+        </h1>
+    </main>
+  )
+}
+
+export default Contents
